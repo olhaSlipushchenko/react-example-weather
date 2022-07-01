2a0ed2d6316bafc23945edb53109131a9500964d
olhaSlipushchenko <103112284+olhaSlipushchenko@users.noreply.github.com>
2022-07-02 00:00:08 +0300

    add open-source link

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://loquacious-queijadas-5abb13.netlify.app/index.html"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Olha Slipushchenko
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/olhaSlipushchenko/react-example-weather.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://loquacious-queijadas-5abb13.netlify.app/index.html"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Olha Slipushchenko
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/olhaSlipushchenko/react-example-weather.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
